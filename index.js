5ff6742ac4fc9d00e94f4d6b8ac23d59537d876d
stablelane <aaanshad09@gmail.com>
2024-10-25 17:15:11 +0530

    call api with language as parameter when a language is chosen by user

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ import { Octokit } from "https://esm.sh/@octokit/core";
 import { key } from "./key.js";
 const dropdownHead = document.getElementById('dropdown-head')
 const dropdownList = document.getElementById('dropdown-list')
-let selectedItem
 
 console.log(key)
 
 // Octokit.js
 // https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-    auth: key
-})
-await octokit.request('GET /search/repositories?q=tetris+language:assembly&sort=stars&order=desc', {
-    headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
-})
+async function getRandomRepo(languageName) {
+    const octokit = new Octokit({
+        auth: key
+    })
+    await octokit.request(`GET /search/repositories?q=language:${languageName}&sort=stars&order=desc`, {
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    .then(data => console.log(data.data.items[Math.round(Math.random() * 30)]))
+}
 
+console.log()
 let languageId = 0
 const res = await fetch('https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json')
 const data = await res.json()
@@ -24,20 +27,20 @@ addListItem(await data)
 
 function addListItem(data) {
     data.forEach(item => {
-                dropdownList.innerHTML += `
-                    <li id="item-${languageId}">${item.value}  </li>
-                `
-            }) 
-            addEventListenerOnList(Object.values(dropdownList.children))
+        dropdownList.innerHTML += `
+        <li id="item-${languageId}">${item.value}<img class="tick-icon" src="icon/checked.png" alt=""></li>
+        `
+    })
+    addEventListenerOnList(Object.values(dropdownList.children))
 
 }
 
 
 function addEventListenerOnList(arr) {
-    
+
     arr.forEach(item => item.addEventListener('click', selectItem))
 
-} 
+}
 
 
 
@@ -46,6 +49,7 @@ dropdownHead.addEventListener('click', showList)
 
 
 
+let selectedItem
 function selectItem(e) {
     if (selectedItem) {
         selectedItem.classList.remove('show-icon')
@@ -56,6 +60,7 @@ function selectItem(e) {
     showList()
     selectedItem = this.children[0]
     selectedItem.classList.add('show-icon')
+    getRandomRepo(this.innerText)
 }
 
 function showList() {
